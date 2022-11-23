2c2171b660da6b62812e4fd40ee6161cc54b27fb
jisungbin <ji@sungb.in>
2022-11-23 21:50:45 +0900

    project init

---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+/*
+ * Developed by 2022 Ji Sungbin
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/duckie-team/DependencyHandlerExtensions/blob/main/LICENSE
+ */
+
+rootProject.name = "DependencyHandlerExtensions"
